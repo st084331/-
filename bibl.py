be6f93eb0764e25233407e120abb19595f5c83b7
st084331 <st084331@student.spbu.ru>
2020-10-02 22:10:08 +0300

    palindrom

--- bibl.py
+++ bibl.py
@@ -18,11 +18,21 @@ class Library:
         print('Удаляем лишние символы из алфавита')
         return alphabet
 
-   # def palindrom(self, alphabet, lenght):
-
+    def palindrom(self, alphabet, length):
+        if (len(alphabet) * 2 < length):
+            return 0
+        else:
+            palindrom_base = alphabet[:length // 2]
+            new_palindrom = palindrom_base
+            if(length % 2 != 0):
+                new_palindrom += alphabet[length // 2 : (length // 2) + 1]
+            new_palindrom += palindrom_base[::-1]
+        print(new_palindrom)
+        return new_palindrom
 
 
 if __name__ == '__main__':
     lib = Library()
-    alphabet = lib.alphabet_len('abcda')
+    alphabet = lib.alphabet_len('abcdef')
+    palindrom = lib.palindrom(alphabet, 7)
 
